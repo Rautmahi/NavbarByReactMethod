c079cbfd64d6e002244aa8cc7fb930755a7914f7
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2022-05-23 22:58:44 +0530

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar =()=>{
+    return (
+
+
+        <div id="navb">
+
+            <div id="left">
+                    <h1>LOGOBAKARY</h1>
+            </div>
+
+            <div id="middle">
+                <p>Services</p>
+                <p>Projects</p>
+                <p>About</p>
+            </div>
+
+        <div id="right">
+            <button>Contact</button>
+        </div>
+
+        </div>
+    )
+}
+export default Navbar
